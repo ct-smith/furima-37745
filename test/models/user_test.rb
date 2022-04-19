ad7758442452d68cd45834a370c8116c1504fd90
ct-smith <melple0212@gmail.com>
2022-04-19 12:24:12 +0900

    バリデーションの設定(新規登録)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  validates :nick_name, presence: true
+  validates :first_name_kanji, presence: true
+  validates :last_name_kanji, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :birth_date, presence: true
+
 end
