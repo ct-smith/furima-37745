35454cbd84c13ff22bccad1e5522063c38b31f99
ct-smith <melple0212@gmail.com>
2022-04-20 14:58:44 +0900

    新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,32 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+          with_options presence: true do
+          validates :nick_name,
+          presence: { message: "  can't be blank"}
+          validates :first_name_kanji,
+          presence: { message: "  can't be blank"},
+          format: { with: /\A[ぁ-んァ-ン一-龥]/, message: ' is invalid. Input full-width characters', allow_blank: true}
+          validates :last_name_kanji,
+          presence: { message:  " can't be blank"},
+          format: { with: /\A[ぁ-んァ-ン一-龥]/, message: ' is invalid. Input full-width characters', allow_blank: true}
+          validates :first_name_kana,
+          presence: { message: "  can't be blank"},
+          format: { with: /\A[ァ-ヶー－]+\z/, message: " is invalid. Input full-width katakana characters", allow_blank: true}
+          validates :last_name_kana,
+          presence: { message: " can't be blank"},
+          format: { with: /\A[ァ-ヶー－]+\z/, message: " is invalid. Input full-width katakana characters", allow_blank: true}
+          validates :birth_date,
+          presence: {message: "  can't be blank"}
+          validates :password,
+          format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/, message: ' is invalid. Include both letters and numbers', allow_blank: true }
+
+         end
+  
+         
+
+         
+
+
+ end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,11 +2,6 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  validates :nick_name, presence: true
-  validates :first_name_kanji, presence: true
-  validates :last_name_kanji, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :birth_date, presence: true
+
 
 end
