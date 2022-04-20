e4cdaa6c351b30040c327424d4dfdcdd17cdda3b
ct-smith <melple0212@gmail.com>
2022-04-20 22:54:38 +0900

    モデルの単体テストコード

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,19 @@
 class ApplicationController < ActionController::Base
-  #before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
 
-
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-    username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]    
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 
   private
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name,:first_name_kanji,:last_name_kanji,:first_name_kana,:last_name_kana,:birth_date ])
-  end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nick_name, :first_name_kanji, :last_name_kanji, :first_name_kana, :last_name_kana,
+                                             :birth_date])
+  end
 end
