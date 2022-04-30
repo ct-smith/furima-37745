786c120918b25d3bc33c4f7fc000a0bd2d56919c
ct-smith <melple0212@gmail.com>
2022-04-30 19:16:46 +0900

    itemコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,6 +51,8 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path unless user_signed_in? && current_user.id == @item.user_id
+     if current_user.id == @item.user_id || @item.purchase.present?
+     redirect_to root_path
+     end
   end
 end
